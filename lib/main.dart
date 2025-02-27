2d929e7c6d993c073b180bfb32640ab208abb92f
Toretay <abdymvzhit@gmail.com>
2025-02-27 15:53:27 +0500

    fetched data from api using http

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +22,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    getPosts();
+    super.initState();
+  }
+
+  void getPosts() async {
+    final response = await http.get(
+      Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+    );
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.white);
